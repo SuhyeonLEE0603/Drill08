36ad6c1b59073206afebc65adfffc5cc150057af
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-05 10:39:17 +0900

    Grass 객체 업데이트 및 렌더링

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,5 +1,6 @@
 from pico2d import *
 
+
 # Game object class here
 class Grass:
     def __init__(self):
@@ -11,8 +12,10 @@ class Grass:
     def update(self):
         pass
 
+
 def handle_events():
     global running
+
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -23,22 +26,27 @@ def handle_events():
 
 def reset_world():
     global running
+    global grass
 
     running = True
+    grass = Grass()
+
 
 def update_world():
+    grass.update()
     pass
 
 
 def render_world():
     clear_canvas()
+    grass.draw()
     update_canvas()
+
 
 open_canvas()
 
 # initialization code
 reset_world()
-
 
 # game main loop code
 while running:
