e83e0f616cec638880ead5aeb899df57c9d3f7f6
Lee Su Hyeon <dltngus12323@gmail.com>
2023-10-05 10:33:24 +0900

    Grass 클래스 생성

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,6 +1,13 @@
 from pico2d import *
 
 # Game object class here
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
 
 def handle_events():
     global running
